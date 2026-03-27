f8eaa375af8b764865aaf7a6d165bc7551a85b19
alexandrwrks <alexandrwrks@gmail.com>
2026-03-27 15:53:13 +0700

    Подготовка к началу работ

---
+++ app/services/main.py
@@ -0,0 +1,14 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+async def main():
+    dp = Dispatcher()
+
+if __name__ == "__main__":
+    asyncio.run(main())
